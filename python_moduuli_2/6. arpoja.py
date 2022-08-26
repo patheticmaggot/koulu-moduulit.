35fe95c3366342559edb5db0e14fda3ccdb0b232
miska <miskasaa@gmail.com>
2022-08-26 15:15:50 +0300

    tehtävä 6 valmis.

---
+++ python_moduuli_2/6. arpoja.py
@@ -0,0 +1,14 @@
+import random
+
+a = (random.randint(0, 9))
+b = (random.randint(0, 9))
+c = (random.randint(0, 9))
+
+print(a, b, c)
+
+d = (random.randint(1, 6))
+e = (random.randint(1, 6))
+f = (random.randint(1, 6))
+g = (random.randint(1, 6))
+
+print(d, e, f, g)
