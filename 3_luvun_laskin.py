5a12e2c62809e2e4d858740d558dcfcd22c6253f
miska <miskasaa@gmail.com>
2022-08-26 13:15:35 +0300

    tehtävä 4 tehty

---
+++ 3_luvun_laskin.py
@@ -0,0 +1,12 @@
+luku1 = float(input('luku 1: '))
+luku2 = float(input('luku 2: '))
+luku3 = float(input('luku 3: '))
+
+summa = luku1+luku2+luku3
+tulo = luku1*luku2*luku3
+keskiarvo = (luku1+luku2+luku3)/3
+
+print('lukujen:')
+print('summa on: '+str(summa))
+print('tulo on: '+str(tulo))
+print('keskiarvo on: '+str(keskiarvo))
