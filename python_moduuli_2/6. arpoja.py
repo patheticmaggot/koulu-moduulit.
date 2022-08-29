67d74677afc556174777df242aa2e5acdff051b2
miska <miskasaa@gmail.com>
2022-08-29 15:10:16 +0300

    lisätty koodeille nimet.

--- python_moduuli_2/6. arpoja.py
+++ python_moduuli_2/6. arpoja.py
@@ -4,11 +4,11 @@ a = (random.randint(0, 9))
 b = (random.randint(0, 9))
 c = (random.randint(0, 9))
 
-print(a, b, c)
+print('Koodi1:', a, b, c)
 
 d = (random.randint(1, 6))
 e = (random.randint(1, 6))
 f = (random.randint(1, 6))
 g = (random.randint(1, 6))
 
-print(d, e, f, g)
+print('Koodi2:', d, e, f, g)
