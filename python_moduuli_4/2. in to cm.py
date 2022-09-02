5bb1190f33226006737810ed27eb95deeb6833c9
miska <miskasaa@gmail.com>
2022-09-02 15:26:42 +0300

    tehtävä 2. muutettu break vapaaksi.

--- python_moduuli_4/2. in to cm.py
+++ python_moduuli_4/2. in to cm.py
@@ -1,8 +1,6 @@
-while True:
+tuuma = float(input('kerro tuumat: '))
+while tuuma > 0:
+    cm = 2.54 * tuuma
+    print(cm)
     tuuma = float(input('kerro tuumat: '))
-    if tuuma > 0:
-        cm = 2.54 * tuuma
-        print(cm)
-    else:
-        break
-print('miinus ei toimi :/')
+print('miinus on rikki :/')
