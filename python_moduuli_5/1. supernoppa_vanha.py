db03644b7bdb1479e8d91472088ff430e9298b4d
miska <miskasaa@gmail.com>
2022-09-07 15:13:51 +0300

    tehty tehtävä 1. for-toistorakenteella.

--- python_moduuli_5/1. supernoppa_vanha.py
+++ python_moduuli_5/1. supernoppa_vanha.py
@@ -1,15 +1,10 @@
 import random
-
 tulokset = []
-tulos = 0
-
 nopat = int(input('monta noppaa haluat heittää: '))
 
 while len(tulokset) < nopat:
     noppa = random.randint(1, 6)
     tulokset.append(noppa)
-
-for n in tulokset:
-    tulos = tulos + n
+tulossumma = sum(tulokset)
 print(tulokset)
-print(tulos)
+print(tulossumma)
