4aba800b57856a2f1682d218913b8d8810b3cde0
miska <miskasaa@gmail.com>
2022-09-07 14:57:48 +0300

    tehty tehtävä 1. ilman for toistoa.

---
+++ python_moduuli_5/1. supernoppa.py
@@ -0,0 +1,10 @@
+import random
+tulokset = []
+nopat = int(input('monta noppaa haluat heittää: '))
+
+while len(tulokset) < nopat:
+    noppa = random.randint(1, 6)
+    tulokset.append(noppa)
+tulossumma = sum(tulokset)
+print(tulossumma)
+print(tulokset)
