0ac2b2d5a676661c2d259f7f6ea46d3171799faf
miska <miskasaa@gmail.com>
2022-09-09 17:42:48 +0300

    tehtävä 6-5. tehty.

---
+++ python_moduuli_ 6/6-5. parittomat pois.py
@@ -0,0 +1,22 @@
+def parittaja(luku):
+    for n in luku:
+        if n % 2 == 0:
+            paritettulista.append(n)
+    return paritettulista
+
+
+lista = []
+paritettulista = []
+
+kokonaisluku = input('anna kokonaisluku tai lopeata entterillä: ')
+
+while kokonaisluku != '':
+    if (kokonaisluku.strip('-')).isnumeric():
+        lista.append(int(kokonaisluku))
+        kokonaisluku = input('anna kokonaisluku tai lopeata enterillä: ')
+    else:
+        print('ei ollut kokonaisluku eikä enter...')
+        kokonaisluku = input('anna kokonaisluku tai lopeata enterillä: ')
+
+tulos = parittaja(lista)
+print(f'tässä parilliset luvut: {tulos}')
