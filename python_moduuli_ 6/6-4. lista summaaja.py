cc200741133c78d683d7fbc78e06e826ea32e97b
miska <miskasaa@gmail.com>
2022-09-09 17:19:00 +0300

    tehtävä 6-4. tehty.

---
+++ python_moduuli_ 6/6-4. lista summaaja.py
@@ -0,0 +1,21 @@
+def summaaja(luku):
+    summa = 0
+    for n in luku:
+        summa += n
+    return summa
+
+
+lista = []
+
+kokonaisluku = input('anna kokonaisluku tai lopeata entterillä: ')
+
+while kokonaisluku != '':
+    if (kokonaisluku.strip('-')).isnumeric():
+        lista.append(int(kokonaisluku))
+        kokonaisluku = input('anna kokonaisluku tai lopeata enterillä: ')
+    else:
+        print('ei ollut kokonaisluku eikä enter...')
+        kokonaisluku = input('anna kokonaisluku tai lopeata enterillä: ')
+
+tulos = summaaja(lista)
+print(tulos)
