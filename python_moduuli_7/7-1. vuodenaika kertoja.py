752843caab1e55a6fa296144468b6618caae12fd
miska <miskasaa@gmail.com>
2022-09-16 02:09:22 +0300

    7-1. tehtävä tehty.

---
+++ python_moduuli_7/7-1. vuodenaika kertoja.py
@@ -0,0 +1,4 @@
+numero = int(input('kerro kuukauden numero (1-12): '))
+vuodenajat = ('talvi', 'talvi', 'kevät', 'kevät', 'kevät', 'kesä', 'kesä', 'kesä', 'syksy', 'syksy', 'syksy', 'talvi')
+vastaus = vuodenajat[numero-1]
+print(vastaus)
