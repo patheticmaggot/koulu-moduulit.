f55254b65d2ccc040cd9c2e41be768d092eea672
miska <miskasaa@gmail.com>
2022-09-16 08:41:55 +0300

    7-1. korjattu.

--- python_moduuli_7/7-1. vuodenaika kertoja.py
+++ python_moduuli_7/7-1. vuodenaika kertoja.py
@@ -1,4 +1,12 @@
 numero = int(input('kerro kuukauden numero (1-12): '))
-vuodenajat = ('talvi', 'talvi', 'kevät', 'kevät', 'kevät', 'kesä', 'kesä', 'kesä', 'syksy', 'syksy', 'syksy', 'talvi')
-vastaus = vuodenajat[numero-1]
-print(vastaus)
+vuodenajat = ('talvi', 'kevät', 'kesä', 'syksy')
+if numero in (1, 2, 12):
+    print(vuodenajat[0])
+elif numero in (3, 4, 5):
+    print(vuodenajat[1])
+elif numero in (6, 7, 8):
+    print(vuodenajat[2])
+elif numero in (9, 10, 11):
+    print(vuodenajat[3])
+else:
+    print('numero ei ole kuukauden numero.')
