1da388f94db36f130a25f5d739f10d06a740118f
Miska Saari <miskajs190799@gmail.com>
2022-10-25 00:46:43 +0300

    9-4. muokattu.

--- python_moduuli_9/9-4. autokilpailu.py
+++ python_moduuli_9/9-4. autokilpailu.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Auto:
     def __init__(self, rekkari, huippunopeus):
         self.rekkari = rekkari
@@ -16,9 +19,17 @@ class Auto:
     def kulje(self, tunnit):
         self.matka += (self.nopeus*tunnit)
 
+
 kilpailijat = []
 
-for k in range(10):
-    kilpailijat.append(f'auto{k}')
+auto(k) = Auto(f'ABC-{k}', hn)
 
+for k in range(1, 11):
+    hn = random.randint(100, 200)
+    kilpailijat.extend([autok = Auto(f'ABC-{k}', hn)])
+
+
+
+for k in kilpailijat:
+    print(k)
 
