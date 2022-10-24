ef6b21fc1a419208abe6adaf709addbeb39e80fe
miska <miskasaa@gmail.com>
2022-10-24 12:39:43 +0300

    9-1. tehty.

---
+++ python_moduuli_9/9-1. auto luokka.py
@@ -0,0 +1,10 @@
+class Auto:
+    def __init__(self,rekkari, huippunopeus):
+        self.rekkari = rekkari
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.matka = 0
+
+auto1 = Auto('ABC-123', 142)
+
+print(f'auton 1: \nrekisteritunnus = {auto1.rekkari} \nhuippunopeus = {auto1.huippunopeus} \nnykyinen nopeus = {auto1.nopeus} \nkuljettu matka = {auto1.matka}')
