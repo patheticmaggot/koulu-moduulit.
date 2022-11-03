07fd15eadec8981c221971c8f4d80050407d0d13
miska <miskasaa@gmail.com>
2022-11-03 14:05:37 +0200

    12-2. parannus

--- python_moduuli_12/12-2. mikä sää.py
+++ python_moduuli_12/12-2. mikä sää.py
@@ -2,9 +2,16 @@ import requests
 
 
 hakusana = input('anna kaupungin nimi: ')
+apikey = '3e7a10de429abd73743b2f470575b9c6'
+units = 'metric'
+language = 'FI'
 
-vastaus = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={hakusana}&APPID=3e7a10de429abd73743b2f470575b9c6&units=metric')
+para = {'appid': apikey, 'units': units, 'lang': language, 'q': hakusana}
+
+vastaus = requests.get(f'https://api.openweathermap.org/data/2.5/weather', para)
+
 if vastaus.ok:
-    print(f'Lämpötila on: {vastaus.json()["main"]["temp"]:.0f}°C')
+    print(f'Säätila: {vastaus.json()["weather"][0]["description"]}')
+    print(f'Lämpötila: {vastaus.json()["main"]["temp"]:.0f}°C')
 else:
     print('haku epäonnistui.')
